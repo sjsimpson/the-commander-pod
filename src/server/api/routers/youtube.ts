d45e7c76bb0bef2f5160a5a27d5781e564e23722
sjsimpson <spencerjsimpson@gmail.com>
2024-02-11 21:04:27 -0700

    formatting

--- src/server/api/routers/youtube.ts
+++ src/server/api/routers/youtube.ts
@@ -6,7 +6,7 @@ export const youtubeRouter = createTRPCRouter({
   getChannel: publicProcedure.query(async () => {
     const apiKey = process.env.GOOGLE_API_KEY;
 
-    const res = await axios.get(
+    const res = await axios.get<YoutubeResponse>(
       "https://www.googleapis.com/youtube/v3/channels",
       {
         headers: {
@@ -23,3 +23,49 @@ export const youtubeRouter = createTRPCRouter({
     return res.data;
   }),
 });
+
+interface YoutubeThumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface YoutubeChannel {
+  title: string;
+  description: string;
+  customUrl: string;
+  publishedAt: string;
+  thumbnails: {
+    default: YoutubeThumbnail;
+    medium: YoutubeThumbnail;
+    high: YoutubeThumbnail;
+  };
+  localized: {
+    title: string;
+    description: string;
+  };
+  country: "US";
+}
+
+interface YoutubeResponseItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: YoutubeChannel;
+  contentDetails: {
+    relatedPlaylists: {
+      likes: string;
+      uploads: string;
+    };
+  };
+}
+
+interface YoutubeResponse {
+  kind: string;
+  etag: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: YoutubeResponseItem;
+}
